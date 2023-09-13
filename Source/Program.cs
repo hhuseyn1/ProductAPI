05cc02a2bdf045898ceaa7687073bb912876c723
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-09-14 01:30:55 +0400

    Add CORS Policy

--- Source/Program.cs
+++ Source/Program.cs
@@ -10,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+ policy.WithOrigins("https://localhost:1234", "http://localhost:7176").AllowAnyHeader().AllowAnyMethod()
+));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -49,7 +53,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
